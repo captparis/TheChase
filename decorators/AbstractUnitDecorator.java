cf03efaf15550aecfe60d3a711e63950b6d3a2e1
Taison Eady <taison.eady@gmail.com>
2016-05-11 20:30:41 +1000

    Implemented the rest of the unit methods in the unit decorators and completed the unit interface.

--- decorators/AbstractUnitDecorator.java
+++ decorators/AbstractUnitDecorator.java
@@ -1,6 +1,8 @@
 
 package decorators;
 
+import javax.swing.ImageIcon;
+
 import models.AbstractUnit;
 import models.Unit;
 
@@ -15,6 +17,44 @@ public abstract class AbstractUnitDecorator implements Unit{
         return unit;
     }
     
+    //declare abstract methods to be changed using decorator.
     public abstract boolean moveable(int x, int y);
     public abstract boolean die(int diceRoll);
+	public abstract ImageIcon getIcon();
+	public abstract Unit clone();
+    
+    //Forward all undecorated method calls to the base unit.
+	public int getInitX() {
+		return unit.getInitX();
+	}
+
+	public int getInitY() {
+		return unit.getInitY();
+	}
+
+	public void setInitX(int x) {
+		unit.setInitX(x);
+	}
+
+	public void setInitY(int y) {
+		unit.setInitY(y);
+	}
+    
+	@Override
+	public String toString() {
+		return unit.toString();
+	}
+
+	public boolean isAlive() {
+		return unit.isAlive();
+	}
+
+	public void setStatus(boolean alive) {
+		unit.setStatus(alive);
+	}
+	
+	public boolean attackable(int x, int y){
+		return unit.attackable(x, y);
+	}
+    
 }
--- models/Unit.java
+++ models/Unit.java
@@ -1,10 +1,32 @@
 
 package models;
 
+import javax.swing.ImageIcon;
+
 public interface Unit {
     
     public abstract boolean moveable(int x, int y);
     
     public abstract boolean die(int diceRoll);
+    
+	public abstract ImageIcon getIcon(); 
+
+	public abstract boolean attackable(int x, int y);
+
+	public abstract int getInitX();
+
+	public abstract int getInitY();
+
+	public abstract void setInitX(int x);
+	
+	public abstract void setInitY(int y);
+
+	public abstract String toString();
+
+	public abstract boolean isAlive();
+
+	public abstract void setStatus(boolean alive);
+        
+	public abstract Unit clone();
     
 }
